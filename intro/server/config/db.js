cfc5da4dc4e9d7a7c5db1258025e7aecb4c88652
Akash Gupta <akashgp9@gmail.com>
2020-09-04 05:52:53 +0530

    Frontend and Backedend: Signup-login page

---
+++ intro/server/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+// Replace this with your MONGOURI.
+const MONGOURI =
+  "mongodb+srv://webdev:webdevteam1@cluster0.jv8em.mongodb.net/node-auth?retryWrites=true&w=majority";
+
+const InitiateMongoServer = async () => {
+  try {
+    await mongoose.connect(MONGOURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to DB !!");
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+module.exports = InitiateMongoServer;
